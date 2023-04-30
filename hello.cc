e5f9af9fa16fd443e49788c9960102f3feb50fbb
Peter <131966585+Petitiii@users.noreply.github.com>
2023-04-30 15:26:54 +0200

    Add files via upload

---
+++ hello.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <unistd.h>
+using namespace std;
+  
+int main()
+{
+    pid_t c_pid = fork();
+  
+    if (c_pid == -1) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+    else if (c_pid > 0) {
+        //  wait(nullptr);
+        cout << "printed from parent process " << getpid() 
+             << endl;
+             int id = getpid();
+             string t = to_string(id);
+             char const *n_char = t.c_str();
+             system("cat /proc/"<<*n_char<<"/status");
+    }
+    else {
+        cout << "printed from child process " << getpid()
+             << endl;
+    }
+    
+    
+  
+    return 0;
+}
