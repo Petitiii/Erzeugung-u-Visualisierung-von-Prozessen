0dc7948e0aa84255826af573e3464a9fb3036e50
Peter <131966585+Petitiii@users.noreply.github.com>
2023-06-06 22:37:38 +0200

    fix

--- hello.cc
+++ hello.cc
@@ -5,56 +5,75 @@
 #include <sys/stat.h>
 #include <fstream>
 #include <string>
-#include <sstream> 
+#include <sstream>
 using namespace std;
 ofstream myFile_Handler;
-  
+
 int main()
 {
-  
-  //Erstellt einen Prozess.
+
+  // Erstellt einen Prozess.
   int pid = fork();
-  //File path um spaeter informationen auslesen zu koennen.
-  string statm_file="/proc/" + to_string(pid) + "/statm";
+  // File path um spaeter informationen auslesen zu koennen.
+  string statm_file = "/proc/" + to_string(pid) + "/statm";
   writeStatus(pid);
 
-  
-  //Oeftnet die Datei
+  // Oeftnet die Datei
   myFile_Handler.open("kp.txt");
 
-
-  char* argument_list[] = { NULL};
+  char *argument_list[] = {NULL};
 
   // Ueberprueft ob das erstellen des Prozesses erfolgreich war.
-  if (pid < 0) {
+  if (pid < 0)
+  {
     // Fehler beim Erstellen des Kind Prozesses
     cout << "Lief Schlecht";
     return 1;
-  } else if (pid == 0) {
+  }
+  else if (pid == 0)
+  {
     // Child process.
-    
+
     std::cout << "Ich Existiere";
     execvp("gnome-terminal", argument_list);
     std::cout << "Das darf nicht ausgegeben werden, wenn alles gut lief";
-
-    
   }
-  //Schliesst die datei
+  // Schliesst die datei
   ifstream statm(statm_file);
-  
+
   writeStatus(pid);
   int size;
   statm >> size;
-  myFile_Handler <<"Size: "<<size <<"lol";
+  myFile_Handler << "Size: " << size << "lol";
   myFile_Handler.close();
 
   return 0;
 }
-void writeStatus(int processid){
-    string status_file = "/proc/" + to_string(processid) + "/status";
-    ifstream status(status_file);
-    string line;
-    while (getline(status, line)) {
-      myFile_Handler << line << endl;
-    }
+void writeStatus(int processid)
+{
+  string status_file = "/proc/" + to_string(processid) + "/status";
+  ifstream status(status_file);
+  string line;
+  while (getline(status, line))
+  {
+    myFile_Handler << line << endl;
   }
+}
+void readStatus()
+{
+  std::ifstream file;
+  std::string line;
+  file.open("kp.txt");
+  if (file.is_open())
+  {
+    while (std::getline(file, line))
+    {
+      std::cout << line << std::endl;
+    }
+    file.close();
+  }
+  else
+  {
+    std::cerr << "Fehler beim öffnen der Datei";
+  }
+}
