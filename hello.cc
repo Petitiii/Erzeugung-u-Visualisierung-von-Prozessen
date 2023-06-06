6e46f51303de690f82cac189c7653bdbec1b5db1
Mokokoooo <131966777+Mokokoooo@users.noreply.github.com>
2023-06-07 01:34:22 +0200

    update

--- hello.cc
+++ hello.cc
@@ -3,26 +3,42 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
 #include <fstream>
 #include <string>
 #include <sstream>
 using namespace std;
 ofstream myFile_Handler;
-void writeStatus(int processid)
+enum{
+  Parent = 0,
+  child
+};
+void writeStatus(int processid, int type)
 {
+  std::ofstream file;
+  if(type == Parent){
+    file.open("ppid.txt");
+  }else{
+  file.open("kp.txt");
+  }
+
   string status_file = "/proc/" + to_string(processid) + "/status";
   ifstream status(status_file);
   string line;
   while (getline(status, line))
   {
-    myFile_Handler << line << endl;
+    file << line << endl;
   }
 }
-void readStatus()
+void readStatus(int type)
 {
   std::ifstream file;
   std::string line;
+    if(type == Parent){
+    file.open("ppid.txt");
+  }else{
   file.open("kp.txt");
+  }
   if (file.is_open())
   {
     while (std::getline(file, line))
@@ -41,40 +57,48 @@ int main()
 {
 
   // Erstellt einen Prozess.
-  int pid = fork();
+  int currpid = getpid();
+  
   // File path um spaeter informationen auslesen zu koennen.
-  string statm_file = "/proc/" + to_string(pid) + "/statm";
+  //string statm_file = "/proc/" + to_string(pid) + "/statm";
   //Schreibt in die Datei 
-  writeStatus(pid);
+  //writeStatus(pid);
 
   // Oeftnet die Datei
-  myFile_Handler.open("kp.txt");
+ 
 
   char *argument_list[] = {NULL};
 
   // Ueberprueft ob das erstellen des Prozesses erfolgreich war.
-  if (pid < 0)
-  {
-    // Fehler beim Erstellen des Kind Prozesses
-    cout << "Lief Schlecht";
-    return 1;
-  }
-  else if (pid == 0)
-  {
-    // Child process.
 
+  if (fork() == -1){
+    sleep(1);
     std::cout << "Ich Existiere";
     execvp("gnome-terminal", argument_list);
     std::cout << "Das darf nicht ausgegeben werden, wenn alles gut lief";
   }
+  
+
+    // Child process.
+  //int childpid = getpid();
+  //std::cout << "childpid:" << childpid << "\n";
+  int pid = getpid();
+  if(currpid == pid){
+  std::cout << "parentid:" << pid << "\n"; 
+  writeStatus(pid, Parent);
+  }else{
+  std::cout << "childpid:" << pid << "\n"; 
+  writeStatus(pid, child);
+  
+  }
+  
+  string statm_file = "/proc/" + to_string(pid) + "/statm";
   // Schliesst die datei
   ifstream statm(statm_file);
 
-  writeStatus(pid);
+
   int size;
   statm >> size;
-  myFile_Handler << "/n Size: " << size;
-  myFile_Handler.close();
 
   return 0;
 }
